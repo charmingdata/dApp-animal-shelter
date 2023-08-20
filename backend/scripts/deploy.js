b2b2c0293e25f7e2502e424fc5838233d2310558
charmingdata <94773218+charmingdata@users.noreply.github.com>
2023-08-20 18:37:29 -0400

    Update deploy.js

--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -1,13 +1,15 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  const shelterContract = await ethers.deployContract("ShelterDB");
+  // this is the correct code. The code shown in the video is incorrect because it only works
+  // with hardhat version 2.16.0 and toolbox version 3. In this tutorial we used 
+  // hardhat version 2.14.0 and toolbox version 2.
+  
+  const shelterContract = await ethers.getContractFactory("ShelterDB");
+  const deployedContract = await shelterContract.deploy();
+  await deployedContract.deployed();
 
-  await shelterContract.waitForDeployment();
-
-  console.log(
-    `Contract deployed to https://testnet-zkevm.polygonscan.com/address/${shelterContract.target}`
-  );
+  console.log("Contract deployed to:", deployedContract.address);
 }
 
 // Call the main function and catch if there is any error
